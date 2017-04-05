63419a87a62ca11023c513adaa3bf770017677d8
Adam DuQuette <duquetteadam@gmail.com>
2017-04-04 17:12:04 -0700

    Finished step two of assignment

--- homework01/main.py
+++ homework01/main.py
@@ -12,8 +12,7 @@ filePath = sys.argv[1]
 # Print success message with file name
 print "Success! File passed:", filePath
 
-def getCsvReader(filePath):
-  file = open(filePath, 'rb')
+def getCsvReader(file):
   dataReader = csv.reader(file)
   return dataReader
 
@@ -22,14 +21,31 @@ def printCsvReader(csvReader):
   for row in csvReader:
     print row
 
+def numOfAmandas(csvReader):
+  # Start counter
+  nAmandas = 0
+
+  # Iterate over dataset
+  for row in csvReader:
+    # If the name field contains 'Amanda' then inc counter
+    if row[7].find("Amanda") > -1:
+      nAmandas += 1
+
+  # Return results
+  return nAmandas
+
+file = open(filePath, 'rb')
 # Open the csv file and get a csv reader object
-dataReader = getCsvReader(filePath)
+dataReader = getCsvReader(file)
 
 # STEP ONE: Read and Print the data
 print "Step 1: Read and Print the data"
 printCsvReader(dataReader)
+file.seek(0) # Seek back to start of file
 
 # STEP TWO: Number of Amandas
 print "Step 2: Number of Amandas"
+print "There are", numOfAmandas(dataReader), "Amandas in this dataset"
+file.seek(0) # Seek back to start of file
 
 
