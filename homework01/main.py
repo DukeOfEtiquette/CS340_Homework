335f534e6b7ad75b515123446ae9ef4c90bd3b8c
Adam DuQuette <duquetteadam@gmail.com>
2017-04-05 17:16:04 -0700

    Step 4 is now complete

--- homework01/main.py
+++ homework01/main.py
@@ -12,14 +12,29 @@ filePath = sys.argv[1]
 # Print success message with file name
 print "Success! File passed:", filePath
 
+# Return a csv reader object based on file path provided
 def getCsvReader(file):
   dataReader = csv.reader(file)
   return dataReader
 
+def getKey(item):
+  return item[1]
+
 # Helper function to print every row in a csv reader object
+# This function and getKey heavily inspired by:
+# http://stackoverflow.com/questions/34472390/how-to-sort-data-alphabetically-in-a-csv-file-created-in-python
 def printCsvReader(csvReader):
+  data = []
+  # Move data into a list
   for row in csvReader:
-    print row
+    data.append(row)
+
+  # Sort list based on the Product column
+  data.sort(key=getKey)
+
+  # Print each row of list
+  for i in data:
+    print i
 
 def numOfAmandas(csvReader):
   # Start counter
@@ -27,7 +42,7 @@ def numOfAmandas(csvReader):
 
   # Iterate over dataset
   for row in csvReader:
-    # If the name field contains 'Amanda' then inc counter
+    # If the name field contains 'amanda' then inc counter
     if row[7].lower().find("amanda") > -1:
       nAmandas += 1
 
@@ -37,17 +52,46 @@ def numOfAmandas(csvReader):
 def avgTransation(csvReader):
   total = 0
   counter = 0
+
   # Iterate over dataset
   for row in csvReader:
+    # Make sure it is a digit
     if row[2].isdigit():
+      # Add to running total, increment counter
       total += float(row[2])
       counter += 1
 
+  # Return the average
   return total/counter
 
 
-file = open(filePath, 'rb')
+def replaceUnitedStates(csvReader):
+  # Make a copy of our reader
+  copyReader = csvReader
+
+  # Open a new csv file and create a write object
+  file = open('USA.csv', 'wb')
+  writer = csv.writer(file)
+
+  # Start a counter variable
+  counter = 0
+
+  # Iterate over reader, making changes as needed
+  for row in copyReader:
+    if row[6].lower().find("united states") > -1:
+      row[6] = "USA"
+      counter += 1 # Increment counter
+
+    # Save row to the writer object
+    writer.writerow(row)
+
+  file.close() # Close new csv file
+
+  return counter
+
+
 # Open the csv file and get a csv reader object
+file = open(filePath, 'rb')
 dataReader = getCsvReader(file)
 
 # STEP ONE: Read and Print the data
@@ -63,3 +107,11 @@ file.seek(0) # Seek back to start of file
 # STEP THREE: Average Transaction Amount
 print "Setp 3: Average Transaction Amount"
 print "Avg:", avgTransation(dataReader)
+file.seek(0) # Seek back to start of file
+
+# STEP FOUR: USA! USA! USA!
+print "Step 4: USA! USA! USA!"
+print replaceUnitedStates(dataReader), "records changed to USA"
+file.seek(0) # Seek back to start of file
+
+file.close() # Close it up
